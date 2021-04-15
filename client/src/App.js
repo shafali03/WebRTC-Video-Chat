26ccf18ff87b81426555576a108278c01ab80eff
shafali03 <shaf03sm@gmail.com>
2021-04-15 15:38:22 +0100

    Created three components videoplayer, notification and options

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
-function App() {
-  return <div>test</div>;
-}
+import React from "react";
+import { Typography, AppBar } from "@material-ui/core";
+
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
+import Notification from "./components/Notification/Notification";
+import Options from "./components/Options/Options";
+
+const App = () => {
+  return (
+    <div>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
+    </div>
+  );
+};
 
 export default App;
